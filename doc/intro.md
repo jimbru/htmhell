10a429211b53a8ada52b79481f33b1bc72ff9ebd
Jim Brusstar <jim.brusstar@gmail.com>
2015-02-11 23:14:49 -0800

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to htmhell
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/htmhell/core_test.clj
@@ -0,0 +1,7 @@
+(ns htmhell.core-test
+  (:require [clojure.test :refer :all]
+            [htmhell.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
