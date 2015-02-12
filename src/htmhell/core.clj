804cfe18583596001e3c40770d867f12b6e2c932
Jim Brusstar <jim.brusstar@gmail.com>
2015-02-12 00:46:56 -0800

    Templating functions

--- src/htmhell/core.clj
+++ src/htmhell/core.clj
@@ -1,4 +1,6 @@
-(ns htmhell.core)
+(ns htmhell.core
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]))
 
 (defn escape
   "Escape an HTML string."
@@ -9,3 +11,24 @@
       (replace ">" "&gt;")
       (replace "\"" "&quot;")
       (replace "'" "&apos;")))
+
+(defn render-string
+  "Renders the string with the given data. Dynamic elements should be wrapped
+  in double curly-braces, like {{this}}. The element's name must be alphanumeric
+  characters only. The data should be a map containing a keyword key matching
+  each dynamic element name."
+  [s data]
+  (string/replace s
+                  #"\{\{\s*(\p{Alnum}*)\s*\}\}"
+                  #(escape (get data (keyword (% 1)) ""))))
+
+(defn read-template
+  "Reads in the template from resources."
+  [n]
+  (slurp (io/resource n)))
+
+(defn render-template
+  "Reads in the template from resources and renders it
+  with the given data (see also `render-string`)."
+  [n data]
+  (render-string (read-template n) data))
--- test/htmhell/core_test.clj
+++ test/htmhell/core_test.clj
@@ -5,3 +5,20 @@
 (deftest escape-test
   (is (= (escape "\"><script>alert();</script>&quot;")
          "&quot;&gt;&lt;script&gt;alert();&lt;/script&gt;&amp;quot;")))
+
+(deftest render-string-test
+  (testing "basic functionality"
+    (let [s "these{{foo}}are making me {{ bar }}"]
+      (is (= (render-string s {:foo " pretzels " :bar "thirsty"})
+             "these pretzels are making me thirsty"))))
+  (testing "missing data"
+    (let [s "{{foo}} now {{bar}}"]
+      (is (= (render-string s {:foo "serenity"})
+             "serenity now "))))
+  (testing "malformed name"
+    (let [s "Doctor {{van-nostrand}}"]
+      (is (= (render-string s {:van-nostrand "xxx"}) s))))
+  (testing "HTML escaping"
+    (let [s "get rid of the {{things}}"]
+      (is (= (render-string s {:things "<b>exclamation points!</b>"})
+             "get rid of the &lt;b&gt;exclamation points!&lt;/b&gt;")))))
